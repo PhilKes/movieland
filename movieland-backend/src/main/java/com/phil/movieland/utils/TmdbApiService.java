e9333421f6eb636c71b8eec8936f6a5d72be9729
Phil Key <39240633+philkes@users.noreply.github.com>
2020-09-26 16:34:53 +0200

    api.properties to get TMDB API Key

--- movieland-backend/src/main/java/com/phil/movieland/utils/TmdbApiService.java
+++ movieland-backend/src/main/java/com/phil/movieland/utils/TmdbApiService.java
@@ -10,6 +10,9 @@ import info.movito.themoviedbapi.model.Video;
 import info.movito.themoviedbapi.model.core.MovieResultsPage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -20,13 +23,15 @@ import java.util.Optional;
  * Service for Tmdb Api interface
  */
 @Service
+@PropertySource(value="classpath:/api.properties", ignoreResourceNotFound=true)
 public class TmdbApiService {
     public static final String POSTER_BASE_URL="https://image.tmdb.org/t/p/w185/";
     public static final String IMAGE_BASE_URL="https://image.tmdb.org/t/p/original";
 
-    private static final String API_KEY="86d157bbb36335ea938b606634e1c4ab";
-    private static final TmdbApi tmdbApi=new TmdbApi(API_KEY);
-    private static final TmdbMovies tmdbMovies=tmdbApi.getMovies();
+    private final String API_KEY;
+
+    private final TmdbApi tmdbApi;
+    private final TmdbMovies tmdbMovies;
 
     private Logger log=LoggerFactory.getLogger(TmdbApiService.class);
 
@@ -37,30 +42,37 @@ public class TmdbApiService {
 
     private static HashMap<Long, String> trailers=new HashMap<>();
 
-    public TmdbApiService() {
+    /**
+     * Create /resources/api.properties with TMDB API-Key
+     */
+    @Autowired
+    public TmdbApiService(@Value("${tmdbApi.apikey}") String apiKey) {
+        API_KEY=apiKey;
+        tmdbApi=new TmdbApi(API_KEY);
+        tmdbMovies=tmdbApi.getMovies();
     }
 
-    public MovieDb getMovieFromTmdb(Movie movie){
-        if(movie.getTmdbId() ==null){
-            MovieResultsPage resultsPage=tmdbApi.getSearch().searchMovie(movie.getName(),null,"en",false,0);
+    public MovieDb getMovieFromTmdb(Movie movie) {
+        if(movie.getTmdbId()==null) {
+            MovieResultsPage resultsPage=tmdbApi.getSearch().searchMovie(movie.getName(), null, "en", false, 0);
             List<MovieDb> results=resultsPage.getResults();
-            if(!results.isEmpty()){
+            if(!results.isEmpty()) {
                 return results.get(0);
             }
         }
-        return tmdbMovies.getMovie(movie.getTmdbId().intValue(),"en");
+        return tmdbMovies.getMovie(movie.getTmdbId().intValue(), "en");
     }
 
-    public List<MovieDb> getMoviesFromTmdb(String name){
-        MovieResultsPage resultsPage=tmdbApi.getSearch().searchMovie(name,null,"en",false,0);
+    public List<MovieDb> getMoviesFromTmdb(String name) {
+        MovieResultsPage resultsPage=tmdbApi.getSearch().searchMovie(name, null, "en", false, 0);
         List<MovieDb> results=resultsPage.getResults();
         return results;
     }
 
-    public List<MovieDb> getTop10Movies(){
+    public List<MovieDb> getTop10Movies() {
 
         //return tmdbApi.getMovies().getTopRatedMovies("en",0).getResults().subList(0,10);
-        return tmdbApi.getMovies().getNowPlayingMovies("en",0,"US").getResults().subList(0, 10);
+        return tmdbApi.getMovies().getNowPlayingMovies("en", 0, "US").getResults().subList(0, 10);
     }
 
     public String getBackdrop(Long movId) {
