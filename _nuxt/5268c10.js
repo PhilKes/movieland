d93f858b7fd0664fe6ec70e15bfb17a5ed00def3
PhilKes <philk97@web.de>
2023-03-12 12:06:26 +0100

    5e38827

---
+++ _nuxt/5268c10.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{744:function(e,n,t){"use strict";t.r(n);var r={middleware:"redirect"},l=t(54),component=Object(l.a)(r,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,null,null);n.default=component.exports}}]);
